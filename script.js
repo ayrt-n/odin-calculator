202ad3b699ffed6f7e7455666d30f6a0967d80db
ayrton <ayrton.parkinson1@gmail.com>
2021-10-21 08:05:55 -0400

    Add EventListener for equals button to call operator function on operands, Add EventListener for the clear button which calls new clear() function that resets calculator, Modify operator functions changing them to an object with methods

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-// Basic Mathematical Operators:
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-// Operator function, takes in two operands and operator
-function operate(a, b, operatorFunction) {
-    return operatorFunction(a, b);
+// Basic Mathematical Operators: Storing functions inside of an object to enable us
+// to call the function with a string i.e., operatorFunction['add'](1,1) for user later on
+operate = {
+    'add' : (a, b) => {return a + b;},
+    'subtract' : (a, b) => {return a - b;},
+    'multiply' : (a, b) => {return a * b;},
+    'divide' : (a, b) => {return a / b;}
 }
 
 
@@ -25,8 +12,8 @@ function operate(a, b, operatorFunction) {
 // Initialize operand, operator, and result variables to be used later
 let operand1 = '';
 let operand2 = '';
-let operator;
-let result;
+let operator = '';
+let result = 0;
 
 let isFirst = true; // Boolean variable to determine whether we are recording op1 or op2
 
@@ -53,17 +40,50 @@ operandButtons.forEach((button) => {
     });
 });
 
-const operators = document.querySelectorAll('.operator');
+const operatorButtons = document.querySelectorAll('.operator');
 
-operators.forEach((operator) => {
-    operator.addEventListener('click', function(e) {
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', function(e) {
         operator = e.target.value
         if (operand1 === '') { // If operand1 is empty, assume equal to 0
             operand1 = '0';
         }
         isFirst = false;
     });
-})
+});
+
+const equalsButton = document.querySelector('.equals');
+
+equalsButton.addEventListener('click', () => {
+    if (operand2 === '') {
+        return;
+    } else if (operator === '') {
+        return;
+    } else { 
+        operand1 = parseInt(operand1);
+        operand2 = parseInt(operand2);
+        result = operate[operator](operand1, operand2);
+        display.textContent = result;
+    }
+    // Reset operand variables, if user wants to enter in new operation
+    operand1 = '';
+    operand2 = '';
+    operator = '';
+    isFirst = true;
+});
+
+const clearButton = document.querySelector('.clear');
+
+function clear() { // Reset all variables
+    operand1 = '';
+    operand2 = '';
+    isFirst = true;
+    operator = '';
+    result = '';
+    display.textContent = 0;
+}
+
+clearButton.addEventListener('click', clear);
 
 
 
